8522a6cde9c75e5aff6772264dda09ef5829cca6
Pedro Callil <pedrocallil@usp.br>
2022-02-02 22:17:09 -0300

    Fixed obabel script.

--- bin/add_charges_and_hydrogen.sh
+++ bin/add_charges_and_hydrogen.sh
@@ -9,8 +9,8 @@ do
 	echo "Now adding charges to system $system"
 	for charge in {none,mmff94,gasteiger};
 	do
-		echo "obabel ${system}_protein_prep.pdb -h --partialcharge $charge -xrh -O  ${system}_receptor_${charge}_hydrogen.pdbqt"
-		echo "obabel ${system}_protein_prep.pdb -d --partialcharge $charge -xr -O  ${system}_receptor_${charge}_hydrogen.pdbqt"
-		echo "obabel ${system}_ligand.sdf --partialcharge $charge -xh -O  ${system}_ligand_${charge}_hydrogen_.pdbqt"
+		obabel ${system}_protein_prep.pdb -h --partialcharge $charge -xrh -O  ${system}_receptor_${charge}_hydrogen.pdbqt
+		obabel ${system}_protein_prep.pdb -d --partialcharge $charge -xr -O  ${system}_receptor_${charge}_hydrogen.pdbqt
+		obabel ${system}_ligand.sdf --partialcharge $charge -xh -O  ${system}_ligand_${charge}_hydrogen_.pdbqt
 	done
 done
