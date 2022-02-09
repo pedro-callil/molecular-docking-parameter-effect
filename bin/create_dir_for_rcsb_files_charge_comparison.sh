ab0b5485ff12fc18d6d07df00e3c4586791c5c11
Pedro Callil <pedrocallil@usp.br>
2022-02-09 16:55:47 -0300

    Script for file selection for docking.

--- bin/create_dir_for_rcsb_files_charge_comparison.sh
+++ bin/create_dir_for_rcsb_files_charge_comparison.sh
@@ -3,10 +3,10 @@
 # This script creates a directory and fills it with all receptor-ligand
 # pairs to which charges and hydrogens atoms were successfully added/removed.
 
-max_sims_ok=7 # 7 different charge models considered; EQEq discarded
+max_sims_ok=4 # 7 different charge models considered; EQEq discarded
 testing_root_dir="$(git rev-parse --show-toplevel)/files_from_rcsb/original_files_and_defaults/"
 ligand_dir="$(git rev-parse --show-toplevel)/files_for_charge_comparison/"
-charge_root_dir="$(git rev-parse --show-toplevel)/files_for_rcsb_charge_comparison/original_files_and_defaults/"
+charge_root_dir="$(git rev-parse --show-toplevel)/files_from_rcsb/files_for_charge_comparison/"
 
 if [ ! -d $charge_root_dir ];
 then
@@ -18,7 +18,7 @@ fi
 for dir in $testing_root_dir/*;
 do
 	sims_ok=$(du $dir/*.pdbqt | grep -v ^0 | wc -l)
-	if [ "$sims_ok" == "$max_sims_ok" ];
+	if [ "$sims_ok" -ge "$max_sims_ok" ];
 	then
 		mkdir $charge_root_dir$(basename $dir)
 		for file in $dir/*.pdbqt;
@@ -30,6 +30,11 @@ do
 		do
 			cp $ligand $charge_root_dir$(basename $dir)/$(basename $ligand)
 		done
+		rm -rf $charge_root_dir$(basename $dir)/*_eqeq*
+		rm -rf $charge_root_dir$(basename $dir)/*_qtpie*
+		rm -rf $charge_root_dir$(basename $dir)/*_eem2015ha*
+		rm -rf $charge_root_dir$(basename $dir)/*_mmff94*
+		rm -rf $charge_root_dir$(basename $dir)/*nohydrogen*
 	fi
 done
 
